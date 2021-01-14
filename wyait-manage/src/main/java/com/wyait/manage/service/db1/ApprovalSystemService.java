1f8ec834276a78220652f475b53bf618848fa224
arthu <asa_don@163.com>
2021-01-14 15:01:00 +0800

    明动审批系统业务类

---
+++ wyait-manage/src/main/java/com/wyait/manage/service/db1/ApprovalSystemService.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (C), 2015-2021, XXX有限公司
+ * FileName: ApprovalSystemService
+ * Author:   Administrator
+ * Date:     2021/1/14
+ * History:
+ */
+package com.wyait.manage.service.db1;
+
+/**
+ * 〈明动审批系统办件对接接口〉
+ *
+ * @author Administrator
+ * @create 2020/12/28 13:44
+ * @since 1.0.0
+ */
+public interface ApprovalSystemService {
+
+    void updateInformation (String controlSeq);
+}
