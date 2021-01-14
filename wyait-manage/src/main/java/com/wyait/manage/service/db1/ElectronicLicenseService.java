5821a01c5e7a7ca4310c94d9bc2fb2a70191bb0a
arthu <asa_don@163.com>
2021-01-14 15:01:21 +0800

    电子证照系统业务类

---
+++ wyait-manage/src/main/java/com/wyait/manage/service/db1/ElectronicLicenseService.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C), 2015-2021, XXX有限公司
+ * FileName: ElectronicLicenseService
+ * Author:   Administrator
+ * Date:     2021/1/14
+ * History:
+ */
+package com.wyait.manage.service.db1;
+
+/**
+ * 〈广东省电子证照系统接口〉
+ *
+ * @author Administrator
+ * @create 2020/12/28 13:50
+ * @since 1.0.0
+ */
+public interface ElectronicLicenseService {
+
+    /**
+     * 登录
+     * @return
+     */
+    String login();
+}
