9248b5afcfac9f650053b1fd221127037cb2aac0
arthu <asa_don@163.com>
2021-01-18 13:10:30 +0800

    新增编码查询

--- wyait-manage/src/main/java/com/wyait/manage/dao/SysIntfParameterDAO.java
+++ wyait-manage/src/main/java/com/wyait/manage/dao/SysIntfParameterDAO.java
@@ -2,9 +2,8 @@ package com.wyait.manage.dao;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.wyait.manage.pojo.SysIntfParameter;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -26,4 +25,14 @@ public interface SysIntfParameterDAO extends BaseMapper<SysIntfParameter> {
      * @return
      */
     int updateByPrimaryKeySelective(SysIntfParameter sysIntfParameter);
+
+    /**
+     *  根据应用系统编码查询
+     * @param encode
+     * @return
+     */
+    @Select("select id,application,ip_port,account,password,secret_key,secret_val,remarks " +
+            " from sys_intf_parameter " +
+            " where enabled = 1 and encoded=#{encode}")
+    SysIntfParameter feachSysParameter(String encode);
 }
